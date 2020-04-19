b78a56ce2180cba8fb89b7bb7da478a2a6e056fa
Peter Dijkstra <pg.dijkstra@caiway.nl>
2020-04-19 08:44:35 +0200

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude , callback) => {
+    url = 'http://api.weatherstack.com/current?access_key=e3a80d9ad4ad95245af4f4f77a245d6c&query=' + latitude + ',' + longitude + '&units=m'
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to Weather Service!', undefined)
+        } else if (body.error) {
+            callback(body.error.info, undefined)
+        } else {
+            callback(undefined, 'The weather is ' + body.current.weather_descriptions[0] + '. The temperature is currently ' + body.current.temperature + ' degrees, but it feels like ' + body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
